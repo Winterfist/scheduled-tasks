a299327cc7ddc25b440a10d327f591c1e676ba50
Winterfist <86793869+Winterfist@users.noreply.github.com>
2026-06-29 15:45:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
+import requests
+from twilio.rest import Client
 import os
-import smtplib
-from datetime import datetime
-import random
-import pandas as pd
-#----------SET UP CONNECTION CREDENTIALS--------------
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
-#----------GET CURRENT DAY AND MONTH-------------------
-today_date_day = datetime.now().day
-today_date_month = datetime.now().month
-#---------GET DATA AND COMPARE-------------------------
-dates = pd.read_csv("birthdays.csv")
-matches = dates[(dates["month"] == today_date_month) & (dates["day"]==today_date_day)]
-#--------CONNECT AND SEND MAIL FOR MATCHES-------------
-for _,match in matches.iterrows():
-    template_file = f"letter_templates/letter_{random.randint(1,3)}.txt"
-    with open(template_file, 'r') as template:
-        letter_content = template.read()
-    personalized_message = letter_content.replace("[NAME]", match["name"])
 
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL, to_addrs=match["email"], msg=f"Subject:Happy birthday!\n\n{personalized_message}")
+account_sid = os.environ["ACCOUNT_SID"]
+auth_token = os.environ["AUTH_TOKEN"]
+
+
+MY_LAT = 46.770920
+MY_LONG = 23.589920
+API_KEY = os.environ["OPEN_WEATHER_API_KEY"]
+
+parameters = {
+        "lat": MY_LAT,
+        "lon": MY_LONG,
+        "appid": API_KEY,
+        "units": "metric",
+        "cnt":4
+    }
+
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+data = response.json()
+forecast_codes = [forecast["weather"][0]["id"] for forecast in data["list"]]
+print(forecast_codes)
+if any(code < 700 for code in forecast_codes):
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="Bring an Umbrella! Rain Announced",
+        from_="+18148217820",
+        to='+40751373733'
+    )
+    print(message.sid)
+else:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="Clear weather today! No need for umbrella",
+        from_="+18148217820",
+        to='+40751373733'
+    )
+    print(message.sid)
